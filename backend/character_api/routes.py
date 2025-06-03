7a342928176df114fef18f178aaa485c5a4b1f53
shahafbr <shahafbrenner@gmail.com>
2025-06-03 13:49:54 +0200

    Q3.0

--- backend/character_api/routes.py
+++ backend/character_api/routes.py
@@ -8,36 +8,53 @@ def hello_world():
 
 @app.route('/characters', methods=['POST'])
 def create_character():
-    data = request.json
-    alias = data['alias']
-    name = data['name']
-    level = data.get('level', 1)
-    health = data.get('health', 100.0)
-    strength = data.get('strength', 5.0)
-    defense = data.get('defense', 5.0)
-    speed = data.get('speed', 5.0)
-
+    alias = request.json['alias']
     if not Character.query.get(alias):
+        name = request.json['name']
         character = Character(alias, name)
-        character.level = level
-        character.health = health
-        character.strength = strength
-        character.defense = defense
-        character.speed = speed
-
         db.session.add(character)
         db.session.commit()
         return format_character(character)
     else:
-        return "Character already exists", 400
+        return "Character already exists"
+
+@app.route('/characters', methods=['GET'])
+def get_characters():
+    characters = Character.query.all()
+    return {'characters': [format_character(character) for character in characters]}
+
+@app.route('/characters/<string:alias>', methods=['GET'])
+def get_character(alias):
+    character = Character.query.get(alias)
+    return format_character(character)
+
+@app.route('/characters/<string:alias>', methods=['PUT'])
+def update_character(alias):
+    character = Character.query.get(alias)
+    if not character:
+        return "Character not found"
+    else:
+        character.alias = request.json['alias']
+        db.session.commit()
+        return format_character(character)
+
+@app.route('/characters/<string:alias>', methods=['DELETE'])
+def delete_character(alias):
+    character = Character.query.get(alias)
+    if not character:
+        return "Character not found"
+    else:
+        db.session.delete(character)
+        db.session.commit()
+        return format_character(character)
 
 def format_character(character):
     return {
-        "alias": character.alias,
-        "name": character.name,
-        "level": character.level,
-        "health": character.health,
-        "strength": character.strength,
-        "defense": character.defense,
-        "speed": character.speed
+        'alias' : character.alias,
+        'name' : character.name,
+        'level' : character.level,
+        'health': character.health,
+        'strength': character.strength,
+        'defense': character.defense,
+        'speed': character.speed
     }
